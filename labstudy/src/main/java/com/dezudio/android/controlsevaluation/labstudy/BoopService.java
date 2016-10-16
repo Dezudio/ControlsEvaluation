c6d2d7701bb37552c071693f51b6a6103959b983
Meg Richards <mouse@cmu.edu>
2016-10-16 15:04:43 -0400

    Add sound to lab activity

---
+++ labstudy/src/main/java/com/dezudio/android/controlsevaluation/labstudy/BoopService.java
@@ -0,0 +1,139 @@
+package com.dezudio.android.controlsevaluation.labstudy;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.ToneGenerator;
+import android.os.Handler;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class BoopService extends IntentService {
+    public static final int TIMING_STOPPED = 0;
+    public static final int TIMING_PAUSED = 1;
+    public static final int TIMING_HAPPENING = 2;
+    public static int timingState = TIMING_PAUSED;
+    public static int timeIndex = 0;
+
+    // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+    private static final String ACTION_START = "com.dezudio.android.controlsevaluation.labstudy.action.START_TIMINGS";
+    private static final String ACTION_PAUSE = "com.dezudio.android.controlsevaluation.labstudy.action.STOP_TIMINGS";
+    private static final String ACTION_STOP = "com.dezudio.android.controlsevaluation.labstudy.action.STOP_TIMINGS";
+
+    private static int[] timings = {10, 14, 20, 5, 45};
+
+    public MediaPlayer mp;
+
+    public BoopService() {
+        super("BoopService");
+        handler = new Handler();
+    }
+
+    static ToneGenerator toneG;
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void startTiming(Context context) {
+        Intent intent = new Intent(context, BoopService.class);
+        intent.setAction(ACTION_START);
+        context.startService(intent);
+        toneG = new ToneGenerator(AudioManager.STREAM_ALARM,500);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    // TODO: Customize helper method
+    public static void pauseTiming(Context context) {
+        Intent intent = new Intent(context, BoopService.class);
+        intent.setAction(ACTION_PAUSE);
+        context.startService(intent);
+    }
+
+    public static void stopTiming(Context context) {
+        Intent intent = new Intent(context, BoopService.class);
+        intent.setAction(ACTION_STOP);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_START.equals(action)) {
+                handleActionStart();
+            } else if (ACTION_STOP.equals(action)) {
+                handleActionStop();
+            } else if (ACTION_PAUSE.equals(action)) {
+                handleActionPause();
+            }
+        }
+    }
+
+    /**
+     * Handle action start in the provided background thread with the provided
+     * parameters.
+     */
+    private Handler handler;
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Log.d("ALARM", "BOOOOOOOOOOP");
+
+            toneG.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 200);
+
+            timeIndex++;
+            if (timeIndex < timings.length)
+                handler.postDelayed(this, timings[timeIndex] * 1000);
+            else {
+                Log.d("ALARM", "SESSION OVER");
+                timingState = TIMING_PAUSED;
+            }
+
+        }
+    };
+
+    private void handleActionStart() {
+        timingState = TIMING_HAPPENING;
+
+        timeIndex = 0;
+        handler.postDelayed(runnable, timings[timeIndex]);
+
+    }
+
+
+    /**
+     * Handle action stop in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionStop() {
+        timingState = TIMING_STOPPED;
+
+
+    }
+
+    private void handleActionPause() {
+        timingState = TIMING_PAUSED;
+        timeIndex = timings.length;
+
+
+    }
+
+}
---
+++ labstudy/src/main/java/com/dezudio/android/controlsevaluation/labstudy/SessionContent.java
@@ -0,0 +1,105 @@
+package com.dezudio.android.controlsevaluation.labstudy;
+
+
+import android.content.Context;
+import android.util.Log;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class SessionContent {
+
+    // List of user sessions
+    public static List<SessionContent.SessionItem> ITEMS = new ArrayList<>();
+
+    // User session details
+    public static Map<String, SessionContent.SessionItem> ITEM_MAP = new HashMap<>();
+
+    private static final String SESSIONS_FILENAME = "sessions.txt";
+    private static final String TIMINGS_FILENAME = "timings.txt";
+
+    public SessionContent(Context context) {
+        readFromInternalStorage(context);
+    }
+
+    public void saveToInternalStorage(Context context) {
+        try {
+            FileOutputStream fos = context.openFileOutput(SESSIONS_FILENAME, Context.MODE_PRIVATE);
+            ObjectOutputStream of = new ObjectOutputStream(fos);
+            of.writeObject(ITEMS);
+            of.flush();
+            of.close();
+            fos.close();
+
+            fos = context.openFileOutput(TIMINGS_FILENAME, Context.MODE_PRIVATE);
+            of = new ObjectOutputStream(fos);
+            of.writeObject(ITEM_MAP);
+            of.flush();
+            of.close();
+            fos.close();
+        } catch (Exception e) {
+            Log.e("InternalStorage", e.getMessage());
+        }
+    }
+
+    public void readFromInternalStorage(Context context) {
+        try {
+            FileInputStream fis = context.openFileInput(SESSIONS_FILENAME);
+            ObjectInputStream oi = new ObjectInputStream(fis);
+            ITEMS = (List<SessionItem>) oi.readObject();
+            oi.close();
+
+            fis = context.openFileInput(TIMINGS_FILENAME);
+            oi = new ObjectInputStream(fis);
+            ITEM_MAP = (Map<String, SessionContent.SessionItem>) oi.readObject();
+        } catch (Exception e) {
+            Log.e("InternalStorageII", e.getMessage());
+        }
+
+    }
+
+    public void addItem(Context context, SessionContent.SessionItem item) {
+        ITEMS.add(item);
+        ITEM_MAP.put(item.id, item);
+        saveToInternalStorage(context);
+    }
+
+    private static SessionContent.SessionItem createSessionItem(int position) {
+        return new SessionContent.SessionItem(String.valueOf(position), "Item " + position);
+    }
+
+    private static String makeDetails(int position) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("Details about Item: ").append(position);
+        for (int i = 0; i < position; i++) {
+            builder.append("\nMore details information here.");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * A session item representing one user's session
+     */
+    public static class SessionItem implements Serializable {
+        public final String id;
+        public final String content;
+
+        public SessionItem(String id, String content) {
+            this.id = id;
+            this.content = content;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+}
