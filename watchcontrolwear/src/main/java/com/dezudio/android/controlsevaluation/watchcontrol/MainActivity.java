9de159b12a6563cfb999bc5351984841de452707
Meg Richards <mouse@cmu.edu>
2016-10-28 15:47:12 -0400

    Reduce Activity Cancellation Time to 1 Sec
    Reduces the time to do a cancellation of the activity from 2 secs to 1.

--- watchcontrolwear/src/main/java/com/dezudio/android/controlsevaluation/watchcontrol/MainActivity.java
+++ watchcontrolwear/src/main/java/com/dezudio/android/controlsevaluation/watchcontrol/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends Activity implements
 
 
     /* Delay duration before recording activity */
-    private static final int NUM_SECONDS = 2;
+    private static final int NUM_SECONDS = 1;
 
 
     /* Communication to Mobile app */
